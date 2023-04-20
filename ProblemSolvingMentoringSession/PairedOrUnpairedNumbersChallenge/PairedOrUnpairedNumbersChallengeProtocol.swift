2802e79c607a7136ef1523d12abd90755ec8600c
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2023-04-20 15:43:24 +0300

    Added `PairedOrUnpairedNumbersChallenge`

---
+++ ProblemSolvingMentoringSession/PairedOrUnpairedNumbersChallenge/PairedOrUnpairedNumbersChallengeProtocol.swift
@@ -0,0 +1,12 @@
+// Copyright © 2023 Almost Engineer. All rights reserved.
+
+import Foundation
+
+/// Write a Swift program to find index which numbers more - paired or unpaired.
+/// It should return true - if paired numbers count is greater than unpaired.
+/// It should return false - if paired numbers count is smaller or equal to unpaired.
+/// If list is empty, than answer should be false
+/// BRANCH: paired_or_unpaired_numbers/YOUR_NAME
+protocol PairedOrUnpairedNumbersChallengeProtocol {
+    func solve(list: [Int]) -> Bool
+}
