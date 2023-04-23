166b94532a33ca2c8ed78bd4fc50fba6515392e3
Yefim <efim.yakunin@gmail.com>
2023-04-23 22:15:38 +0300

    Finished SumatorChallenge

--- ProblemSolvingMentoringSession/SumatorChallenge/Solvings/SumatorChallenge_Yakunin.swift
+++ ProblemSolvingMentoringSession/SumatorChallenge/Solvings/SumatorChallenge_Yakunin.swift
@@ -1,11 +1,3 @@
-//
-//  SumatorChallenge_Yakunin.swift
-//  ProblemSolvingMentoringSession
-//
-//  Created by Yefim on 23.04.2023.
-//  Copyright © 2023 Almost Engineer. All rights reserved.
-//
-
 import Foundation
 final class SumatorChallenge_Yakunin: SumatorChallengeProtocol {
     func sum(a: Int, b: Int) -> Int {
