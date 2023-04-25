bb6a8cf04ce8b5a7a0a1092647b7967ec969b5c6
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2023-04-25 16:09:08 +0300

    Added `CalculateHowMany22InArrayChallenge`

---
+++ ProblemSolvingMentoringSession/Part 0/CalculateHowMany22InArrayChallenge/CalculateHowMany22InArrayChallengeProtocol.swift
@@ -0,0 +1,12 @@
+// Copyright © 2023 Almost Engineer. All rights reserved.
+
+import Foundation
+
+/// Write a Swift program for getting a count of number 22 in input. Return 1000 if no 22 in array
+/// if input [1, 22, 22, 22, 10], result 3
+/// if input [], result 1000
+/// if input [1, 23, 2], result 1000
+/// BRANCH: check_bigger_18/YOUR_NAME
+protocol CalculateHowMany22InArrayChallengeProtocol {
+    func solve(input: [Int]) -> Int
+}
