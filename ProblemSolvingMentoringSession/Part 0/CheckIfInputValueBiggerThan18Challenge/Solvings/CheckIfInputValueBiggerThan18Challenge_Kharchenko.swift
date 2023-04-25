d3ccffa3ff7cc92979bf9b18637dfefc2aa8d2db
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-04-25 18:22:06 +0200

    Deleted comment

--- ProblemSolvingMentoringSession/Part 0/CheckIfInputValueBiggerThan18Challenge/Solvings/CheckIfInputValueBiggerThan18Challenge_Kharchenko.swift
+++ ProblemSolvingMentoringSession/Part 0/CheckIfInputValueBiggerThan18Challenge/Solvings/CheckIfInputValueBiggerThan18Challenge_Kharchenko.swift
@@ -1,10 +1,6 @@
 import Foundation
 
 final class CheckIfInputValueBiggerThan18Challenge_Kharchenko: CheckIfInputValueBiggerThan18ChallengeProtocol {
-    /// Напишите программу Swift для возврата true, если входное значение больше 18
-    /// если вход 2, результат false
-    /// если вход 19, результат истина
-    /// если вход 18, результат false
     func solve(input: Int) -> Bool {
         if input > 18 {
             return true
