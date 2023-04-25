0e0fc6f0ad4c17f4b69b247a94ef858d5db25ef3
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2023-04-25 16:02:08 +0300

    Added `ReturnFirstPairNumberInArrayChallenge`

---
+++ ProblemSolvingMentoringSession/Part 0/ReturnFirstPairNumberInArrayChallenge/ReturnFirstPairNumberInArrayChallengeProtocol.swift
@@ -0,0 +1,13 @@
+// Copyright © 2023 Almost Engineer. All rights reserved.
+
+import Foundation
+
+/// Write a Swift program for returning first paired element in given array if exists
+/// if input [1, 2, 10, 1, 2, 4, 8, 0], result 2
+/// if input [1, 1, 1, 3, 7, 200], result 200
+/// if input [], result nil
+/// if input [1, 1, 3], result nil
+/// BRANCH: check_bigger_18/YOUR_NAME
+protocol ReturnFirstPairNumberInArrayChallengeProtocol {
+    func solve(input: [Int]) -> Int?
+}
