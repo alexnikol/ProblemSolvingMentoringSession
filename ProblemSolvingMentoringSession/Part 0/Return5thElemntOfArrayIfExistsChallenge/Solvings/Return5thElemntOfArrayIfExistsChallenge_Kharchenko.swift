1817ff7f4d1e908a6d639a9789abe72f5a75956d
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-04-26 17:35:10 +0200

    Improved func

--- ProblemSolvingMentoringSession/Part 0/Return5thElemntOfArrayIfExistsChallenge/Solvings/Return5thElemntOfArrayIfExistsChallenge_Kharchenko.swift
+++ ProblemSolvingMentoringSession/Part 0/Return5thElemntOfArrayIfExistsChallenge/Solvings/Return5thElemntOfArrayIfExistsChallenge_Kharchenko.swift
@@ -2,7 +2,10 @@ import Foundation
 
 final class Return5thElemntOfArrayIfExistsChallenge_Kharchenko: Return5thElemntOfArrayIfExistsChallengeProtocol {
     func solve(input: [Int]) -> Int? {
-        let index = input[5]
-        return index
+        if input.count > 5 {
+            return input[5]
+        } else {
+            return nil
+        }
     }
 }
