ce3440fa3932872050eb0e0ea38ce42fc76ddedd
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-04-26 11:17:06 +0200

    Finished "CheckNumberInRangeOf12And24" challenge

---
+++ ProblemSolvingMentoringSession/Part 0/CheckNumberInRangeOf12And24Challenge/Solvings/CheckNumberInRangeOf12And24Challenge_Kharchenko.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+final class CheckNumberInRangeOf12And24Challenge_Kharchenko: CheckNumberInRangeOf12And24ChallengeProtocol {
+    
+    func solve(input: Int) -> Bool {
+        if input == 12 {
+            return true
+        } else if input == 13 {
+            return true
+        } else if input == 14 {
+            return true
+        } else if input == 15 {
+            return true
+        } else if input == 16 {
+            return true
+        } else if input == 17 {
+            return true
+        } else if input == 18 {
+            return true
+        } else if input == 19 {
+            return true
+        } else if input == 20 {
+            return true
+        } else if input == 21 {
+            return true
+        } else if input == 22 {
+            return true
+        } else if input == 23 {
+            return true
+        } else if input == 24 {
+            return true
+        } else {
+            return false
+        }
+    }
+}
