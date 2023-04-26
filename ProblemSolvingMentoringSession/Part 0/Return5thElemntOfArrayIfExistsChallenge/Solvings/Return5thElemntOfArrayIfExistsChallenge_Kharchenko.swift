69294fc80500681c7b259e5f97f0a7a8f35febff
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-04-26 13:34:44 +0200

    Finished "Return5thElemntOfArrayIfExists" challenge

---
+++ ProblemSolvingMentoringSession/Part 0/Return5thElemntOfArrayIfExistsChallenge/Solvings/Return5thElemntOfArrayIfExistsChallenge_Kharchenko.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+final class Return5thElemntOfArrayIfExistsChallenge_Kharchenko: Return5thElemntOfArrayIfExistsChallengeProtocol {
+    func solve(input: [Int]) -> Int? {
+        let index = input[5]
+        return index
+    }
+}
