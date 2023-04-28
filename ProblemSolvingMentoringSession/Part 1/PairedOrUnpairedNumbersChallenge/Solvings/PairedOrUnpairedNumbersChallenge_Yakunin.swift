fa4988a9430f465b4e6a5ab6cc01de584a6e4d60
Yefim <efim.yakunin@gmail.com>
2023-04-28 14:10:05 +0300

    Finished PairedOrUnpairedNumbersChallenge

---
+++ ProblemSolvingMentoringSession/Part 1/PairedOrUnpairedNumbersChallenge/Solvings/PairedOrUnpairedNumbersChallenge_Yakunin.swift
@@ -0,0 +1,30 @@
+//
+//  PairedOrUnpairedNumbersChallenge_Yakunin.swift
+//  ProblemSolvingMentoringSession
+//
+//  Created by Yefim on 28.04.2023.
+//  Copyright © 2023 Almost Engineer. All rights reserved.
+//
+
+import Foundation
+final class PairedOrUnpairedNumbersChallenge_Yakunin: PairedOrUnpairedNumbersChallengeProtocol {
+    func solve(list: [Int]) -> Bool {
+        guard !list.isEmpty else { return false }
+        var pairedCont = 0
+        var unpairedCount = 0
+        
+        for item in list {
+            if item % 2 == 0 {
+                pairedCont += 1
+            } else {
+                unpairedCount += 1
+            }
+        }
+        
+        if pairedCont > unpairedCount {
+            return true
+        } else {
+            return false
+        }
+    }
+}
