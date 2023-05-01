179c59bb1ba96898830a35d63d5708bf585b442d
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-05-01 17:46:53 +0200

    Saved

--- ProblemSolvingMentoringSession/Part 0/ArrayValidatorChallenge/Solvings/ArrayValidatorChallenge_Kharchenko.swift
+++ ProblemSolvingMentoringSession/Part 0/ArrayValidatorChallenge/Solvings/ArrayValidatorChallenge_Kharchenko.swift
@@ -3,22 +3,23 @@ import Foundation
 final class ArrayValidatorChallenge_Kharchenko: ArrayValidatorChallengeProtocol {
     func solve(input: [Int]) -> String {
         /// Написать программу Swift, возвращающую проверочное сообщение о массиве со следующими правилами
-        /// если на входе более 3 элементов, результат "READY_TO_WORK"
         /// если на входе есть хотя бы 2 числа 13 и больше 5, результат "PREPARED_LIST"
         /// если в других случаях возвращается "WAITING"
         guard !input.isEmpty else { return "DISABLED" }
-        
-        var resultString = String()
-        
-        for _ in input {
-            if input.count > 5 &&  {
-                return "READY_TO_WORK"
-            } else if input.count > 3  {
-                return "PREPARED_LIST"
-            } else {
-                return "WAITING"
+        var sum13 = 0
+        for number in input {
+            if number == 13 {
+                sum13 += 1
             }
         }
-        return resultString
+        if sum13 >= 2 && input.count > 5 {
+            return "PREPARED_LIST"
+        }
+        if input.count > 3 {
+            return "READY_TO_WORK"
+        }
+        return "WAITING"
     }
 }
+
+//[13, 13, 1, 1, 1, 1]
