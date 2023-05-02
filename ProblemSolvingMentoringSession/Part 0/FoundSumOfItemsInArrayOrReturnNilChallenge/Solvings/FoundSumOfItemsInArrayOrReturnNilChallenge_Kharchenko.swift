2501567abe108c889529633813ff1d350a6fcd12
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-05-02 21:09:39 +0200

    Used for to calculate the amount

--- ProblemSolvingMentoringSession/Part 0/FoundSumOfItemsInArrayOrReturnNilChallenge/Solvings/FoundSumOfItemsInArrayOrReturnNilChallenge_Kharchenko.swift
+++ ProblemSolvingMentoringSession/Part 0/FoundSumOfItemsInArrayOrReturnNilChallenge/Solvings/FoundSumOfItemsInArrayOrReturnNilChallenge_Kharchenko.swift
@@ -4,8 +4,8 @@ final class FoundSumOfItemsInArrayOrReturnNilChallenge_Kharchenko: FoundSumOfIte
     func solve(input: [Int]) -> Int? {
         guard !input.isEmpty else { return nil }
         var sumArray = 0
-        for number in 0..<input.count {
-            sumArray += input[number]
+        for number in input {
+            sumArray += number
         }
         return sumArray
     }
