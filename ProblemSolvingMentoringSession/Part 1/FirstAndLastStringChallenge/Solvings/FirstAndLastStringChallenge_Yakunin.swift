461e18eea27fd72ef961fa32bc7077e6c1309da0
Yefim <efim.yakunin@gmail.com>
2023-05-04 21:32:26 +0300

    first commit

---
+++ ProblemSolvingMentoringSession/Part 1/FirstAndLastStringChallenge/Solvings/FirstAndLastStringChallenge_Yakunin.swift
@@ -0,0 +1,9 @@
+//
+//  FirstAndLastStringChallenge_Yakunin.swift
+//  ProblemSolvingMentoringSession
+//
+//  Created by Yefim on 04.05.2023.
+//  Copyright © 2023 Almost Engineer. All rights reserved.
+//
+
+import Foundation
