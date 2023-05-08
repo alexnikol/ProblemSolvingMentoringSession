efa3d4f8d0255f9e610af1ad801c76082fac128d
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-05-08 19:09:46 +0200

    Removed empty lines

--- ProblemSolvingMentoringSession/Part 1/PairedOrUnpairedNumbersChallenge/Solvings/PairedOrUnpairedNumbersChallenge_Kharchenko.swift
+++ ProblemSolvingMentoringSession/Part 1/PairedOrUnpairedNumbersChallenge/Solvings/PairedOrUnpairedNumbersChallenge_Kharchenko.swift
@@ -3,10 +3,8 @@ import Foundation
 final class PairedOrUnpairedNumbersChallenge_Kharchenko: PairedOrUnpairedNumbersChallengeProtocol {
     func solve(list: [Int]) -> Bool {
         guard !list.isEmpty else { return false }
-        
         var pairedResult = 0
         var unpairedResult = 0
-        
         for number in list {
             if number % 2 == 0 {
                 pairedResult += 1
