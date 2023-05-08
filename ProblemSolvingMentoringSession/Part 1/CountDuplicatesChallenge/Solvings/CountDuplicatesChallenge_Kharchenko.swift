db97ba7e43ec646c7ec3262a407fa1421cf9ad44
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-05-08 15:04:38 +0200

    Finished "CountDuplicates" challenge

--- ProblemSolvingMentoringSession/Part 1/CountDuplicatesChallenge/Solvings/CountDuplicatesChallenge_Kharchenko.swift
+++ ProblemSolvingMentoringSession/Part 1/CountDuplicatesChallenge/Solvings/CountDuplicatesChallenge_Kharchenko.swift
@@ -2,6 +2,13 @@ import Foundation
 
 final class CountDuplicatesChallenge_Kharchenko: CountDuplicatesChallengeProtocol {
     func solve(list: [Int], searchedItem: Int) -> Int {
-        return 0
+        guard !list.isEmpty else { return 0 }
+        var result = 0
+        for number in list {
+            if number == searchedItem {
+                result += 1
+            }
+        }
+        return result
     }
 }
