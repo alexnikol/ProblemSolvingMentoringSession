f453c7f7006a6036fee917763fbf8a5f0615fd7d
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-05-09 22:38:30 +0200

    Сhanged to a more accurate name

--- ProblemSolvingMentoringSession/Part 1/FirstIndexOfSearchedItemChallenge/Solvings/FirstIndexOfSearchedItemChallenge_Kharchenko.swift
+++ ProblemSolvingMentoringSession/Part 1/FirstIndexOfSearchedItemChallenge/Solvings/FirstIndexOfSearchedItemChallenge_Kharchenko.swift
@@ -3,13 +3,13 @@ import Foundation
 final class FirstIndexOfSearchedItemChallenge_Kharchenko: FirstIndexOfSearchedItemChallengeProtocol {
     func solve(age: Int, workers: [Worker]) -> Int? {
         guard !workers.isEmpty else { return nil }
-        var searchedWorker : Int?
+        var searchWorkerIndex : Int?
         for (index, worker) in workers.enumerated() {
             if worker.age == age {
-                searchedWorker = index
+                searchWorkerIndex = index
                 break
             }
         }
-        return searchedWorker
+        return searchWorkerIndex
     }
 }
