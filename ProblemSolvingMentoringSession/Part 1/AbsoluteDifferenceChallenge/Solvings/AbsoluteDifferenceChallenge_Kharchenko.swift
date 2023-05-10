4f8835d78544a5ececd6174fb03c68471f9dc8c6
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-05-10 15:39:02 +0200

    Finished "AbsoluteDifference" challenge

---
+++ ProblemSolvingMentoringSession/Part 1/AbsoluteDifferenceChallenge/Solvings/AbsoluteDifferenceChallenge_Kharchenko.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+final class AbsoluteDifferenceChallenge_Kharchenko: AbsoluteDifferenceChallengeProtocol {
+    func solve(_ n: Int) -> Int {
+        var result = Int()
+        
+        if n == 0 {
+            return 51
+        }
+        if n != 0 && n < 51 {
+            result = 51 - n
+        } else {
+            result = n - 51
+            result *= 2
+        }
+        return result
+    }
+}
+
