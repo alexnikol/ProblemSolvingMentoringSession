fa73a9f67fb11a1e1cac5af2c84974a56d9fd608
potemkin123 <vladyurchenko04@gmail.com>
2023-05-10 21:12:29 +0300

    Calculate22 done

--- ProblemSolvingMentoringSession/Part 0/CalculateHowMany22InArrayChallenge/Solvings/CalculateHowMany22InArrayChallenge_Yurchenko.swift
+++ ProblemSolvingMentoringSession/Part 0/CalculateHowMany22InArrayChallenge/Solvings/CalculateHowMany22InArrayChallenge_Yurchenko.swift
@@ -9,12 +9,15 @@
 import Foundation
 final class CalculateHowMany22InArrayChallenge_Yurchenko: CalculateHowMany22InArrayChallengeProtocol {
     func solve(input: [Int]) -> Int {
-        guard !input.isEmpty else { return 1000 }
-        guard input.contains(22) else { return 1000 }
-           var result = 0
-           for item in input where item == 22 {
-               result += 1
-           }
-           return result
-       }
+        var result = 0
+        for item in input {
+            if item == 22 {
+                result += 1
+            }
+        }
+        if result == 0 {
+            return 1000
+        }
+        return result
     }
+}
