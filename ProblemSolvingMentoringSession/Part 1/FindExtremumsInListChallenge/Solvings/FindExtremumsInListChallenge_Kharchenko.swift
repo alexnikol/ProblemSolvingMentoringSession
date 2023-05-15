618ffb48208cfdb1dbf4b59f4642acf18c8768c1
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-05-15 21:15:01 +0200

    Taking a number by index

--- ProblemSolvingMentoringSession/Part 1/FindExtremumsInListChallenge/Solvings/FindExtremumsInListChallenge_Kharchenko.swift
+++ ProblemSolvingMentoringSession/Part 1/FindExtremumsInListChallenge/Solvings/FindExtremumsInListChallenge_Kharchenko.swift
@@ -3,21 +3,18 @@ import Foundation
 final class FindExtremumsInListChallenge_Kharchenko: FindExtremumsInListChallengeProtocol {
     func solve(list: [Int]) -> FindExtremumsInListChallengeResult {
         guard !list.isEmpty else { return FindExtremumsInListChallengeResult(minimum: nil, maximum: nil) }
-        var minNumber = 0
-        var maxNumber = 0
+        var minNumber = list[0]
+        var maxNumber = list[0]
         
         for number in list {
-            if list.count < 2 {
-                minNumber = number
-                maxNumber = number
-            }
-            else if number < minNumber {
+            if number < minNumber {
                 minNumber = number
             }
             else if maxNumber < number {
                 maxNumber = number
             }
         }
+        
         let result = FindExtremumsInListChallengeResult(minimum: minNumber, maximum: maxNumber)
         return result
     }
