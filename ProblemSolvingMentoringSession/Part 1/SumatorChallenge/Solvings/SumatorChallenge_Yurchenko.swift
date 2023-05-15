e8e8caf957c25c833b16cdab06da49771e6adae6
potemkin123 <vladyurchenko04@gmail.com>
2023-05-15 20:03:50 +0300

    Sumator done

---
+++ ProblemSolvingMentoringSession/Part 1/SumatorChallenge/Solvings/SumatorChallenge_Yurchenko.swift
@@ -0,0 +1,14 @@
+//
+//  SumatorChallenge_Yurchenko.swift
+//  ProblemSolvingMentoringSession
+//
+//  Created by Владислав Юрченко on 15.05.2023.
+//  Copyright © 2023 Almost Engineer. All rights reserved.
+//
+
+import Foundation
+final class SumatorChallenge_Yurchenko: SumatorChallengeProtocol {
+    func sum(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
