2a3424ab306b0d8bf89c4f963060560ba851caaa
Maksym Bilyk <black.bro1401@gmail.com>
2023-05-16 12:17:43 +0300

    IntegersAcceptorChallenge_Bilyk - fixed problems

--- ProblemSolvingMentoringSession/Part 1/IntegersAcceptorChallenge/Solvings/IntegersAcceptorChallenge_Bilyk.swift
+++ ProblemSolvingMentoringSession/Part 1/IntegersAcceptorChallenge/Solvings/IntegersAcceptorChallenge_Bilyk.swift
@@ -5,10 +5,6 @@ import Foundation
 final class IntegersAcceptorChallenge_Bilyk: IntegersAcceptorChallengeProtocol {
     
     func solve(value1: Int, value2: Int) -> Bool {
-        if value1 == 20 || value2 == 20 || value1 + value2 == 20 {
-            return true
-        } else {
-            return false
-        }
+        value1 == 20 || value2 == 20 || value1 + value2 == 20
     }
 }
