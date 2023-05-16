94b159ec8ddd215569b3549d4bc10b6901b4039c
potemkin123 <vladyurchenko04@gmail.com>
2023-05-16 20:07:36 +0300

    IntegerAcceptor redone

--- ProblemSolvingMentoringSession/Part 1/IntegersAcceptorChallenge/Solvings/IntegersAcceptorChallenge_Yurchenko.swift
+++ ProblemSolvingMentoringSession/Part 1/IntegersAcceptorChallenge/Solvings/IntegersAcceptorChallenge_Yurchenko.swift
@@ -13,14 +13,6 @@
 import Foundation
 final class IntegersAcceptorChallenge_Yurchenko: IntegersAcceptorChallengeProtocol{
     func solve(value1: Int, value2: Int) -> Bool {
-        if value1 == 20 {
-            return true
-        } else if value2 == 20 {
-            return true
-        } else if value1 + value2 == 20 {
-            return true
-        } else {
-            return false
-        }
+        return value1 == 20 || value2 == 20 || value1 + value2 == 20
     }
 }
