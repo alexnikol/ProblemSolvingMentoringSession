0effa05f2e5609c2067097e0c53470f19c4aa0fe
AlexNikol <alexnikol@jatapp.com>
2023-05-19 10:03:46 +0300

    Added `CheckCountOrExtremum`

---
+++ ProblemSolvingMentoringSessionTests/Part 2/CheckCountOrExtremumTests.swift
@@ -0,0 +1,24 @@
+//
+//  CheckCountOrExtremumTests.swift
+//  ProblemSolvingMentoringSessionTests
+//
+//  Created by PC-587 on 19.05.2023.
+//  Copyright © 2023 Almost Engineer. All rights reserved.
+//
+
+import XCTest
+@testable import ProblemSolvingMentoringSession
+
+final class CheckCountOrExtremumTests: XCTestCase {
+    private var solvings: [CheckCountOrExtremumProtocol] = []
+    
+    func test_solvings() {
+        solvings.forEach { assertThat(solving: $0) }
+    }
+    
+    private func assertThat(solving: CheckCountOrExtremumProtocol, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertEqual(solving.solve(list: []), false)
+        XCTAssertEqual(solving.solve(list: [1, 2, 5, 10, 2, 4, 3]), false)
+        XCTAssertEqual(solving.solve(list: [1, 1, 3, 1]), true)
+    }
+}
