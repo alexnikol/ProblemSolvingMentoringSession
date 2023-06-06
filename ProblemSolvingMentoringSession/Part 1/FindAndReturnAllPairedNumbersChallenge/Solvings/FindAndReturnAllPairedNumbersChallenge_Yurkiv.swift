f454c608b650a22cbfcc369aed87801a844bdba8
petroyurkiv <igorlamativ@gmail.com>
2023-06-06 15:28:48 +0300

    FindAndReturnAllPairedNumbersChallenge done

---
+++ ProblemSolvingMentoringSession/Part 1/FindAndReturnAllPairedNumbersChallenge/Solvings/FindAndReturnAllPairedNumbersChallenge_Yurkiv.swift
@@ -0,0 +1,19 @@
+//
+//  FindAndReturnAllPairedNumbersChallenge_Yurkiv.swift
+//  ProblemSolvingMentoringSession
+//
+//  Created by Petro on 06.06.2023.
+//  Copyright © 2023 Almost Engineer. All rights reserved.
+//
+
+import Foundation
+
+final class FindAndReturnAllPairedNumbersChallenge_Yurkiv: FindAndReturnAllPairedNumbersChallengeProtocol {
+    
+    func solve(list: [Int]) -> [Int] {
+        list.filter { element in
+            guard element % 2 == 0 else { return false }
+            return true
+        }
+    }
+}
