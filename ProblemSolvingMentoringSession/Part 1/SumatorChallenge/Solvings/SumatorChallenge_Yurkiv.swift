53320b9eba8777d8e188952c10a765a2d769c7a0
petroyurkiv <igorlamativ@gmail.com>
2023-06-08 19:37:27 +0300

    SumatorChallenge done

---
+++ ProblemSolvingMentoringSession/Part 1/SumatorChallenge/Solvings/SumatorChallenge_Yurkiv.swift
@@ -0,0 +1,15 @@
+//
+//  SumatorChallenge_Yurkiv.swift
+//  ProblemSolvingMentoringSession
+//
+//  Created by Petro on 08.06.2023.
+//  Copyright © 2023 Almost Engineer. All rights reserved.
+//
+
+import Foundation
+
+final class SumatorChallenge_Yurkiv: SumatorChallengeProtocol {
+    func sum(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
